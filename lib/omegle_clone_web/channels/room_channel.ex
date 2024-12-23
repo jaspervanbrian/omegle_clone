9dde6912c1f7a9f4b91ba92b3039ca131137dd81
Jasper Martin <jaspervanbrianmartin@gmail.com>
2024-12-23 18:22:55 +1100

    Setup room channel.

--- lib/omegle_clone_web/channels/room_channel.ex
+++ lib/omegle_clone_web/channels/room_channel.ex
@@ -4,59 +4,79 @@ defmodule OmegleCloneWeb.RoomChannel do
   require Logger
 
   @impl true
-  def join("room:lobby", payload, socket) do
-    if authorized?(payload) do
-      {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
-    end
-  end
-
-  @impl true
   def join("room:" <> room_id, payload, socket) do
     if authorized?(payload) do
-      # :ok = Room.connect(room_id, self())
-      {:ok, assign(socket, :room_id, room_id)}
+      pid = self()
+      send(pid, :after_join)
+
+      case Nexus.RoomRegistryServer.create_room(room_id) do
+        {:ok, _room_pid} ->
+          case Room.add_peer(room_id, pid) do
+            {:ok, peer_id} ->
+              {:ok, assign(socket, %{peer_id: peer_id, room_id: room_id})}
+
+            {:error, _reason} = error -> error
+          end
+
+        {:error, reason} ->
+          Logger.warning("Error on peer_channel:")
+          {:error, %{reason: reason}}
+      end
+
     else
       {:error, %{reason: "unauthorized"}}
     end
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
   @impl true
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (room:lobby).
-  @impl true
-  def handle_in("shout", payload, socket) do
-    broadcast(socket, "shout", payload)
+  def handle_in("sdp_answer", %{"body" => body}, socket) do
+    :ok = Peer.apply_sdp_answer(socket.assigns.peer, body)
     {:noreply, socket}
   end
 
   @impl true
-  def handle_in("signaling", msg, socket) do
-    :ok = Room.receive_signaling_msg(socket.assigns.room_id, msg)
+  def handle_in("sdp_offer", %{"body" => _body}, socket) do
+    # TODO: renegotiate
+    Logger.warning("Ignoring SDP offer sent by peer #{socket.assigns.peer}")
     {:noreply, socket}
   end
 
   @impl true
-  def handle_info({:session_time, session_time}, socket) do
-    push(socket, "sessionTime", %{time: session_time})
+  def handle_in("ice_candidate", %{"body" => body}, socket) do
+    Peer.add_ice_candidate(socket.assigns.peer, body)
     {:noreply, socket}
   end
 
   @impl true
-  def handle_info(:session_expired, socket) do
-    {:stop, {:shutdown, :session_expired}, socket}
+  def handle_cast({:offer, sdp_offer}, socket) do
+    push(socket, "sdp_offer", %{"body" => sdp_offer})
+    {:noreply, socket}
   end
 
   @impl true
-  def terminate(reason, _socket) do
-    Logger.info("Stopping Phoenix chnannel, reason: #{inspect(reason)}.")
+  def handle_cast({:candidate, candidate}, socket) do
+    push(socket, "ice_candidate", %{"body" => candidate})
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(:after_join, socket) do
+    {:ok, _ref} = Presence.track(socket, socket.assigns.peer, %{})
+    push(socket, "presence_state", Presence.list(socket))
+    {:noreply, socket}
+  end
+
+  @impl true
+  def terminate(_reason, socket) do
+    # Optional: Clean up supervisor if this was the last connection
+    # You might want to track connection counts per topic first
+    if ((socket |> Presence.list |> Enum.count) - 1) <= 0 do
+      room_id = socket.assigns.room_id
+
+      Nexus.RoomRegistryServer.terminate_room(room_id)
+    end
+
+    {:noreply, socket}
   end
 
   # Add authorization logic here as required.
